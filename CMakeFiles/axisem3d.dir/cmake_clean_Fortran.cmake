681c1833a928c9d3c721a84de53df3165b7cd499
benjaminfernando <benjfernando@gmail.com>
2019-01-15 11:31:23 +0000

    Full Fluid at increased time precision

---
+++ CMakeFiles/axisem3d.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,14 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "s20rts.mod"
+  "S20RTS.mod"
+  "CMakeFiles/axisem3d.dir/s20rts.mod.stamp"
+
+  "s40rts.mod"
+  "S40RTS.mod"
+  "CMakeFiles/axisem3d.dir/s40rts.mod.stamp"
+
+  "simplex.mod"
+  "SIMPLEX.mod"
+  "CMakeFiles/axisem3d.dir/simplex.mod.stamp"
+  )
